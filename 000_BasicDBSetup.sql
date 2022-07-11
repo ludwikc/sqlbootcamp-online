c16e756a3e01e8820ca224a7e162a28ea9cf8996
Ludwik C. Siadlak <ludwikc@users.noreply.github.com>
2022-07-11 12:51:04 +0200

    Update 000_BasicDBSetup.sql

--- 000_BasicDBSetup.sql
+++ 000_BasicDBSetup.sql
@@ -9,6 +9,7 @@
   
   
  -- Check if the SQLBootcampDB exits, and if not - create one.
+ USE [master]
  
  IF NOT EXISTS
     (SELECT * FROM sys.databases WHERE name = 'SQLBootcampDB')
@@ -16,6 +17,10 @@
     CREATE DATABASE [SQLBootcampDB]
    END
  GO
+ 
+ -- Set Recovery Model to SIMPLE, regardless of Model DB settings
+ USE [master]
+ ALTER DATABASE [SQLBootcampDB] SET RECOVERY SIMPLE WITH NO_WAIT
  
  -- Switch the context to SQLBootcampDB
  USE [SQLBootcampDB]
